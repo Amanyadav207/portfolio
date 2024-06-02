c420d17b3e9d2331cbf8cb50d5b85d992eef34e0
Amanyadav207 <aman.23bcs10055@ms.sst.scaler.com>
2024-06-03 00:18:25 +0530

    portfolio

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,20 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hi I'm Aman Yadav, a Full Stack Developer.
+            i am a full stack developer with experience in building web applications using modern technologies like React, Node.js, Express.js, and MongoDB. 
+            I have a passion for building scalable and responsive web applications that provide a great user experience.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
